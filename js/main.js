4cd18a17530e5115fb41721514be86d8526f5eb1
Mohd Sahil <sahilstack@gmail.com>
2021-11-08 11:36:47 +0530

    complete home page

--- js/main.js
+++ js/main.js
@@ -9,3 +9,19 @@ window.addEventListener('scroll', function() {
     let header = document.querySelector('.nav-bar');
     header.classList.toggle('scrolling-active', window.scrollY > 200);
 })
+
+$(document).ready(function() {
+    $("#testimonial-slider").owlCarousel({
+        items: 1,
+        itemsDesktop: [1000, 2],
+        itemsDesktopSmall: [979, 1],
+        itemsTablet: [768, 1],
+        pagination: false,
+        navigation: true,
+        slideSpeed: 1000,
+        singleItem: true,
+        transitionStyle: "goDown",
+        navigationText: ["", ""],
+        autoPlay: false
+    });
+});
